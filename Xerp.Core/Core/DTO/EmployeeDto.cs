b99410605013bda068a9520f1cb21fad3b156691
jicking <7297029+jicking@users.noreply.github.com>
2022-09-19 14:41:02 +0800

    Add core code lib

---
+++ Xerp.Core/Core/DTO/EmployeeDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xerp.Core.DTO;
+public class EmployeeDto
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MiddleName { get; set; }
+    public DateTime BirthDate { get; set; }
+}
---
+++ Xerp.Core/Core/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xerp.Core.Entities;
+
+public abstract class BaseEntity
+{
+    Guid Id { get; set; }
+    // meta fields
+    string? CreatedBy { get; set; }
+    DateTimeOffset? DateCreated { get; set; }
+    string? LastModifiedBy { get; set; }
+    DateTimeOffset? DateLastModified { get; set; }
+    string? DeletedBy { get; set; }
+    DateTimeOffset? DateDeleted { get; set; }
+    bool IsDeleted { get; set; }
+}
