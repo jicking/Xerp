cff0a8b6c9ef655cb4fc14eebed305d8d8ee3730
jicking <7297029+jicking@users.noreply.github.com>
2022-09-19 19:54:00 +0800

    resolve tast failure

--- Xerp.Core/Core/Services/AuthTokenService.cs
+++ Xerp.Core/Core/Services/AuthTokenService.cs
@@ -1,4 +1,5 @@
 using RestSharp;
+using System.Text.Json;
 using System.Threading;
 
 namespace Xerp.Core.Services;
@@ -10,13 +11,15 @@ public class Auth0TokenService : IAuthTokenService {
 
 	public async Task<AuthToken> GetTestAuthTokenAsync() {
 		var client = new RestClient("https://jickingdev.us.auth0.com/oauth");
-		var request = new RestRequest("/token", Method.Post);
+		var request = new RestRequest("/token");
 		request.AddHeader("content-type", "application/json");
 		request.AddParameter("application/json", "{\"client_id\":\"hxc3B3CXbrXDskMh7WDxYhBxsxYzwrSN\",\"client_secret\":\"wyt6E3aX4gkMYf3XHazuG0W1PU3LBF6ecyGSyFK_tWeJCFZ3eoWdFqjxwsL1TWmr\",\"audience\":\"xerp-service\",\"grant_type\":\"client_credentials\"}", ParameterType.RequestBody);
-		var response = await client.GetAsync(request, CancellationToken.None);
+		var response = await client.PostAsync(request, CancellationToken.None);
 
-		if (response.IsSuccessful)
-			return null;
+		if (response.IsSuccessful) {
+			var token = JsonSerializer.Deserialize<AuthToken>(response.Content);
+			return token;
+		}
 
 		return null;
 	}
