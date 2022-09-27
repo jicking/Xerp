494d3179262e174f691f3dcdb5ab367a20f1022d
jicking bebiro <7297029+jicking@users.noreply.github.com>
2022-09-27 11:25:29 -0400

    x

---
+++ Xerp/Pages/App/EmployeeManagement.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Xerp.Pages.App
+{
+    public class EmployeeManagementModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
